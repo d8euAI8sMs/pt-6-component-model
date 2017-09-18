f79626fc9284d62a1cadcb97f1f18466f7ffb6ea
kalaider <a.kalaider@yandex.ru>
2017-09-18 11:41:33 +0300

    Some documentation provided

--- ComponentDll/ComponentRegistration.h
+++ ComponentDll/ComponentRegistration.h
@@ -4,6 +4,44 @@
 
 #include "ComponentDll.h"
 
+/**
+ * summary:
+ *
+ *      Manages component installation by manipulating the
+ *      registry keys.
+ *
+ * remarks:
+ *
+ *      The component should be registered in the Windows
+ *      Registry in the following way:
+ *
+ *      (Optional)
+ *      HKEY_CLASSES_ROOT\<ProgIdVerInd> = <Name>
+ *      HKEY_CLASSES_ROOT\<ProgIdVerInd>\CurVer = <ProgId> (Optional)
+ *      HKEY_CLASSES_ROOT\<ProgIdVerInd>\CLSID = <CLSID> (Optional)
+ *
+ *      (Optional)
+ *      HKEY_CLASSES_ROOT\<ProgId> = <Name>
+ *      HKEY_CLASSES_ROOT\<ProgId>\CurVer = <ProgId> (Possibly other) (Optional)
+ *      HKEY_CLASSES_ROOT\<ProgId>\CLSID = <CLSID>
+ *
+ *      HKEY_CLASSES_ROOT\CLSID\<CLSID> = <Name> (Optional)
+ *      HKEY_CLASSES_ROOT\CLSID\<CLSID>\ProgID = <ProgId> (Optional)
+ *      HKEY_CLASSES_ROOT\CLSID\<CLSID>\VersionIndependentProgID = <ProgIdVerInd> (Optional)
+ *      HKEY_CLASSES_ROOT\CLSID\<CLSID>\InprocServer32 = <PathToDll>
+ *      HKEY_CLASSES_ROOT\CLSID\<CLSID>\InprocServer32\ThreadingModel = <ThreadingModel> (Optional)
+ *
+ *      Here we use Apartment threading model since no explicit
+ *      thread synchronization of component internal variables
+ *      was implemented.
+ *
+ * see also:
+ *
+ *      https://www.codeproject.com/Articles/338268/COM-in-C
+ *      https://msdn.microsoft.com/en-us/library/ms809971.aspx
+ *      https://www.codeproject.com/Articles/9190/Understanding-The-COM-Single-Threaded-Apartment-Pa
+ */
+
 namespace ComponentDll
 {
     namespace Impl
