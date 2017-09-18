d3fa88b2e7bd26e3c8a1c562948796f50bb97cba
kalaider <a.kalaider@yandex.ru>
2017-09-18 14:55:22 +0300

    DllMain now sets curent module variable

--- ComponentDll/dllmain.cpp
+++ ComponentDll/dllmain.cpp
@@ -1,5 +1,13 @@
 #include "stdafx.h"
 
+namespace ComponentDll
+{
+    namespace Impl
+    {
+        extern HMODULE g_hCurrentModule;
+    }
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -8,6 +16,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        ComponentDll::Impl::g_hCurrentModule = hModule;
+        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
